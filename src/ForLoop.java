fac92c66fbce806fa803f690538abd46eaa1c3f9
arifin <arifin.ptsb@gmail.com>
2024-10-14 22:14:55 +0800

    #add java fundamental 1.Looping 2.Tenary Operation

---
+++ src/ForLoop.java
@@ -0,0 +1,28 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        //infiniteLoop();
+        conditionLoop();
+        initStatementLoop();
+
+    }
+
+    private static void initStatementLoop() {
+        for (int counter = 1; counter < 10; counter++) {
+            System.out.println("Hello World with initStatement loop" + counter);
+        }
+    }
+
+    private static void conditionLoop() {
+        var counter = 1;
+        for (; counter < 10; ) {
+            System.out.println("Hello World with infinite loop" + counter);
+            counter++
+        }
+    }
+
+    private static void infiniteLoop() {
+        for (; ; ) {
+            System.out.println("Hello World with infinite loop");
+        }
+    }
+}
---
+++ src/TenaryOperator.java
@@ -0,0 +1,8 @@
+public class TenaryOperator {
+    public static void main(String[] args) {
+        var nilai = 75;
+        String ucapan = nilai >= 75 ? "Selamat Anda Lulus Ujian" : "Silahkan Coba lagi";
+        System.out.println(ucapan);
+
+    }
+}
